bd67b8dc0ffd086ce31988b648d58a0f7c102a5d
Finnbar Home <91543127+FinnbarHome@users.noreply.github.com>
2024-09-24 15:26:16 +0100

    formatting

--- backend/routes/discounts.js
+++ backend/routes/discounts.js
@@ -26,11 +26,9 @@ router.get("/discounts/:discountID", async (req, res) => {
   try {
     const discountID = parseInt(req.params.discountID);
     const discount = await db.collection("Discounts").findOne({ "DiscountID": discountID });
-    
     if (!discount) {
       return res.status(404).json({ error: "Discount not found" });
     }
-    
     res.json(discount);
   } catch (error) {
     handleError(res, error);
@@ -41,17 +39,14 @@ router.get("/discounts/:discountID", async (req, res) => {
 router.post("/discounts", async (req, res) => {
   try {
     const { DiscountID, Company, LevelReq, DiscountCode, Description } = req.body;
-
     // Check if the DiscountID already exists
     const existingDiscount = await db.collection("Discounts").findOne({ DiscountID });
     if (existingDiscount) {
       return res.status(400).json({ error: "A discount with the same DiscountID already exists" });
     }
-
     // Insert the new discount into the collection
     const newDiscount = { DiscountID, Company, LevelReq, DiscountCode, Description };
     const result = await db.collection("Discounts").insertOne(newDiscount);
-    
     res.status(201).json(result.ops[0]); // Return the newly created discount
   } catch (error) {
     handleError(res, error);
