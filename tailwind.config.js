35b79eb5e405a9e93cb3655f6ebc65e4bbaade0d
Finnbar Home <91543127+FinnbarHome@users.noreply.github.com>
2024-10-07 22:11:16 +0100

    Tail wind comments remv

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './public/**/*.html', // This will include all HTML files in the public folder and its subfolders
-    './public/**/*.js',   // This includes any JavaScript files in the public folder
+    './public/**/*.html', 
+    './public/**/*.js',  
   ],
   theme: {
     extend: {},
